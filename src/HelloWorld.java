eb2ead03ad5d6f590709193e8c735960751a3152
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-05-12 21:02:39 -0500

    notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,65 @@
+
+
 public class HelloWorld {
 
+
+    //main method
+    public static void main(String[] args) {
+
+        // datatype variableName
+
+        double salaryRequirement;
+        boolean isEmployed;
+
+        // Single line variable declaration
+        int yearFounded = 2011;
+        String name = "Mono";
+        int age = 85;
+
+        // int declaration and assignment
+        int numberOfPrimitiveTypes = 8;
+
+
+        // doubles can have decimal places:
+        double price = 8.99;
+        double  androidShare = 81.7;
+
+
+        int number = 5;
+
+        number += 3; // Value is now 8
+        number -= 4; // Value is now 4
+        number *= 6; // Value is now 24
+        number /= 2; // Value is now 12
+        number %= 7; // Value is now 5
+
+
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World");
+
+        /*MULITLINE COMMENT
+        James Gosling is a Canadian engineer who
+		created Java while working at Sun Microsystems.
+		His favorite number is the square root of 2!
+        */
+
+        // single line
+        System.out.print("Today is a great day to code!");
+        System.out.print("same line");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        System.out.println(name);
+        System.out.println(yearFounded);
+        System.out.println(age);
+        System.out.println(age +" " + name + " " + yearFounded);
+        System.out.println(numberOfPrimitiveTypes);
+        System.out.println(androidShare);
+    }
+
+
+
+
 }
+
+
